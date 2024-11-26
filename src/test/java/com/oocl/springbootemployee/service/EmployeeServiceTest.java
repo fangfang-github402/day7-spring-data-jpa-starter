c1261b94d36119844a4d6685c6a63235970339db
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-26 17:47:38 +0800

    test:should created employee active when create employee

--- src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
@@ -76,18 +76,16 @@ class EmployeeServiceTest {
         assertThrows(EmployeeAgeNotValidException.class, () -> employeeService.create(kitty));
         verify(mockedEmployeeRepository, never()).save(any());
     }
-//
-//    @Test
-//    void should_created_employee_active_when_create_employee() {
-//        //given
-//        EmployeeRepository mockedEmployeeRepository = mock(EmployeeRepository.class);
-//        EmployeeService employeeService = new EmployeeService(mockedEmployeeRepository);
-//        Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
-//        //when
-//        employeeService.create(lucy);
-//        /* then */
-//        verify(mockedEmployeeRepository).create(argThat(Employee::getActive));
-//    }
+
+    @Test
+    void should_created_employee_active_when_create_employee() {
+        //given
+        Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
+        //when
+        employeeService.create(lucy);
+        /* then */
+        verify(mockedEmployeeRepository).save(argThat(Employee::getActive));
+    }
 //
 //    @Test
 //    void should_throw_EmployeeAgeSalaryNotMatchedException_when_save_given_a_employee_with_age_over_30_and_salary_below_20K() {
